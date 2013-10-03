4cf5acff1ce50c9c932ebb05d41057d6739e0132
angeagu <angeagu@gmail.com>
2013-10-03 19:51:10 +0200

    Versión 2.0 Facturas.
    Añadida funcionalidad facturas por Mes/Año y Todas.
    Añadida funcionalidad de búsqueda de facturas.
    Mejora de las transiciones entre pestañas.

--- PhoneGapTestProject/assets/www/facturas/PhoneGapTestFacturas.js
+++ PhoneGapTestProject/assets/www/facturas/PhoneGapTestFacturas.js
@@ -1,10 +1,32 @@
 
 document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("backbutton", onBackKeyDown, false);
 
 function onDeviceReady() {
 	
 	var db = window.openDatabase("FACTURAS", "1.0", "Facturas", 1000000);
 	db.transaction(poblarBBDD,errorBBDD,exitoBBDD);
+	
+	var fecha = new Date();
+	var dia = fecha.getDate();
+	var mes = fecha.getMonth() + 1;
+	if (mes<10) {mes = "0" + mes;}
+	var ano = fecha.getFullYear();
+	
+	alert (dia+'-'+mes+'-'+ano);
+	
+	var selectDia = $('#select-dia');
+	var selectMes = $('#select-mes');
+	var selectAno = $('#select-ano');
+	
+	selectDia.val(dia).attr('selected', true).siblings('option').removeAttr('selected');
+	selectMes.val(mes).attr('selected', true).siblings('option').removeAttr('selected');
+	selectAno.val(ano).attr('selected', true).siblings('option').removeAttr('selected');
+
+	selectDia.selectmenu("refresh", true);
+	selectMes.selectmenu("refresh", true);
+	selectAno.selectmenu("refresh", true);
+	
 }
 
 function poblarBBDD(transaction) {
@@ -23,25 +45,42 @@ function poblarBBDD(transaction) {
 	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Supermercado","123.45","27-06-2012")'); 
 	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Taller Coche","98.87","27-06-2012")');
 	
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Gas","123.45","27-07-2012")'); 
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Alquiler","98.87","27-05-2012")');
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("IBI","123.45","27-06-2012")'); 
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Agua","98.87","27-07-2012")');
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Dinero Cajero","123.45","27-05-2012")'); 
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Comisiones","98.87","27-06-2012")');
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Telefono","123.45","27-07-2012")'); 
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Iberdrola","98.87","27-05-2012")');
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Supermercado","123.45","27-06-2012")'); 
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Taller Coche","98.87","27-06-2012")');
+	
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Gas","123.45","27-07-2013")'); 
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Alquiler","98.87","27-05-2013")');
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("IBI","123.45","27-06-2013")'); 
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Agua","98.87","27-07-2013")');
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Dinero Cajero","123.45","27-05-2013")'); 
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Comisiones","98.87","27-06-2013")');
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Telefono","123.45","27-07-2013")'); 
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Iberdrola","98.87","27-05-2013")');
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Supermercado","123.45","27-06-2013")'); 
+	transaction.executeSql('INSERT INTO FACTURAS (CONCEPTO,IMPORTE,FECHA) VALUES ("Taller Coche","98.87","27-06-2013")');
+		
+	//transaction.executeSql("SELECT * FROM FACTURAS", [], returnRows, errorBBDD);
+	cargarFacturasMes(0);
 	
 }
 
-function selectFacturas(transaction) {
-	console.log('ejecutandoSelect');
-	transaction.executeSql('SELECT * FROM FACTURAS', [], returnRows, errorBBDD);
-	
-}
 
 function returnRows(transaction,results) {
 	console.log("Num filas devueltas: " + results.rows.length);
 	//alert("Num filas devueltas: " + results.rows.length);
-	//if (!results.rowsAffected) {
+
 	if (results.rows.length==0) {
 	    console.log('No hay filas afectadas!');
-	    return false;
 	}
-	  // for an insert statement, this property will return the ID of the last inserted row
-	  //console.log("Ultimo id insertado = " + results.insertId);
+	  
 	crearTabla(results);
 
 }
@@ -49,6 +88,16 @@ function returnRows(transaction,results) {
 
 function crearTabla(results) {
 	$('#listaregistros').empty();
+	
+	//Añadimos la cabecera
+	$('#listaregistros').append('<div class="ui-block-a ui-bar-a">CONCEPTO</div>');
+	$('#listaregistros').append('<div class="ui-block-b ui-bar-a">IMPORTE</div>');
+	$('#listaregistros').append('<div class="ui-block-c ui-bar-a">FECHA</div>');
+	$('#listaregistros').append('<div class="ui-block-a ui-bar-a"></div>');
+	$('#listaregistros').append('<div class="ui-block-b ui-bar-a"></div>');
+	$('#listaregistros').append('<div class="ui-block-c ui-bar-a"></div>');
+	
+	alert("NumFacturas: " + results.rows.length);
 	for (var i=0; i<results.rows.length; i++) {
 		var row = results.rows.item(i);
 		console.log("Fila = " + i + " CONCEPTO = " + row.CONCEPTO + " Importe =  " + row.IMPORTE + " Fecha: " + row.FECHA);
@@ -62,14 +111,14 @@ function crearTabla(results) {
 
 
 function guardarRegistro() {
-	alert('entrando en guardar registro');
+	//alert('entrando en guardar registro');
 	$('#formularioRegistro');
 	var concepto = $('#concepto').val();
 	var dia=$('#select-dia').val();
 	var mes=$('#select-mes').val();
 	var ano=$('#select-ano').val();
 	var fecha = dia + '-' + mes + '-' + ano;
-	alert(fecha);
+	//alert(fecha);
 	var importe = $('#importe').val();
 	
 	var db = window.openDatabase("FACTURAS", "1.0", "Facturas", 1000000);
@@ -84,42 +133,103 @@ function insertaFactura (transaction,concepto,importe,fecha) {
 	console.log('Sql a ejecutar: ' + sql);
 	transaction.executeSql(sql);
 	console.log('fin inserta factura');
+	
+	transaction.executeSql("SELECT * FROM FACTURAS", [], returnRows, errorBBDD);
 }
 
 
-function errorBBDD(error) {
-	alert ('Error al procesar SQL: ' + error);
-	console.log('Error al procesar SQL: ' + error)
-}
 
-
-function exitoBBDD(error) {
-	console.log('Ejecucion de sql correcta');
+function cargarFacturasMes(mes) {
+	//alert ('Entrando en cargarFacturasMes');
+	var fecha;
+	var month=mes;
+	var year;
+	if (month==0) {
+		fecha = new Date();
+		month = fecha.getMonth()+1;
+		if (month<10) {
+			month = "0" + month;
+		}
+		year=fecha.getFullYear();
+	}
+	
 	var db = window.openDatabase("FACTURAS", "1.0", "Facturas", 1000000);
-	db.transaction(selectFacturas,errorBBDD);
+	db.transaction(function(transaction) {
+					queryFacturasMes(transaction,month,year);
+					},
+					errorBBDD,exitoBBDD);
+	
 }
 
-function cargarFacturasMes() {
-	alert('cargarFacturasMes');
+function queryFacturasMes(transaction,month,year) {
+	var sql= 'SELECT * FROM FACTURAS WHERE FECHA LIKE "%-' + month + '-' + year +'"';
+	alert("SQL:" + sql);
+	transaction.executeSql(sql, [], returnRows, errorBBDD);
 }
 
-function cargarFacturasAno() {
-	alert('cargarFacturasAno');
+function cargarFacturasAno(anno) {
+	//alert ('Entrando en cargarFacturasAno');
+	var fecha;
+	var year=anno;
+	if (year==0) {
+		fecha = new Date();
+		year = fecha.getFullYear();
+	}
+	
+	var db = window.openDatabase("FACTURAS", "1.0", "Facturas", 1000000);
+	db.transaction(function(transaction) {
+					queryFacturasAno(transaction,year);
+					},
+					errorBBDD,exitoBBDD);
+	
+}
+
+function queryFacturasAno(transaction,year) {
+	var sql= 'SELECT * FROM FACTURAS WHERE FECHA LIKE "%-' + year + '"';
+	transaction.executeSql(sql, [], returnRows, errorBBDD);
+}
+
+function buscarFacturas() {
+	alert('Entrando en Buscar Facturas');
+	
+	var db = window.openDatabase("FACTURAS", "1.0", "Facturas", 1000000);
+	db.transaction(function(transaction){
+		transaction.executeSql('SELECT * FROM FACTURAS', [], crearListaBusquedaFacturas, errorBBDD);
+	},errorBBDD);
+	
+}
+
+function crearListaBusquedaFacturas(transaction,results) {
+
+	var listaBusquedaFacturas = $('#listabusquedafacturas');
+	listaBusquedaFacturas.empty();
+	
+	
+	alert("NumFacturas: " + results.rows.length);
+	for (var i=0; i<results.rows.length; i++) {
+		var row = results.rows.item(i);
+		console.log("Fila = " + i + " CONCEPTO = " + row.CONCEPTO + " Importe =  " + row.IMPORTE + " Fecha: " + row.FECHA);
+		var txt = row.CONCEPTO + "            " + row.IMPORTE + "        " + row.FECHA;
+		listaBusquedaFacturas.append('<li><a href="" data-role="button">'+txt+'</a></li>');
+	}
+	
 }
 
 function cargarListaCompleta() {
 	alert('Entrando en cargarListaCompleta');
 	console.log('Entrando en cargarListaCompleta');
+	
 	var db = window.openDatabase("FACTURAS", "1.0", "Facturas", 1000000);
 	db.transaction(function(transaction){
 		transaction.executeSql('SELECT * FROM FACTURAS', [], crearTablaListaCompleta, errorBBDD);
 	},errorBBDD);
+	
 }
 
 
 function crearTablaListaCompleta(transaction,results) {
-	console.log("Num filas devueltas: " + results.rows.length);
-	//alert("Num filas devueltas: " + results.rows.length);
+	//console.log("Num filas devueltas: " + results.rows.length);
+	alert("Num filas devueltas: " + results.rows.length);
 	//if (!results.rowsAffected) {
 	if (results.rows.length==0) {
 	    console.log('No hay filas afectadas!');
@@ -128,18 +238,34 @@ function crearTablaListaCompleta(transaction,results) {
 	  // for an insert statement, this property will return the ID of the last inserted row
 	  //console.log("Ultimo id insertado = " + results.insertId);
 	crearTabla(results);
-	$('#listacompletaregistros').empty();
-	$('#listacompletaregistros').append('<ul data-role="listview" data-filter="true" data-inset="true">');
-	for (var i=0; i<results.rows.length; i++) {
-		var row = results.rows.item(i);
-		console.log("Fila = " + i + " CONCEPTO = " + row.CONCEPTO + " Importe =  " + row.IMPORTE + " Fecha: " + row.FECHA);
-		$('#listacompletaregistros').append('<li>');
-		//$('#listacompletaregistros').append('<div class="ui-block-a ui-bar-a">'+row.CONCEPTO +'</div>');
-		//$('#listacompletaregistros').append('<div class="ui-block-b ui-bar-a">'+row.IMPORTE +'</div>');
-		//$('#listacompletaregistros').append('<div class="ui-block-c ui-bar-a">'+row.FECHA +'</div>');
-		$('#listacompletaregistros').append('<a href="#">'+row.CONCEPTO + '  ' + row.FECHA + '  ' + row.IMPORTE + '  ' + '</a>');
-		$('#listacompletaregistros').append('</li>');
+	
+}
 
-	}
-	$('#listacompletaregistros').append('</ul>');
+function onBackKeyDown()  {
+	navigator.notification.confirm(
+       ("Desea salir de la aplicacion?"),
+        saliraplicacion,
+        'PhoneGap Facturas',
+        'YES,NO');
+ 	
+}
+
+function saliraplicacion(button) {
+   //alert('entrando con button: ' + button);
+   if(button=="1" || button==1) {
+    navigator.app.exitApp();
+   }
+}
+
+
+function errorBBDD(error) {
+	alert ('Codigo de Error al obtener transaccion SQL: ' + error.code);
+	alert ('Mensaje de Error al obtener transaccion SQL: ' + error.message);
+	
+}
+
+
+function exitoBBDD(transaction) {
+	alert('Transaccion obtenida correctamente');
+	console.log('Transaccion obtenida correctamente');
 }
